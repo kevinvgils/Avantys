1695bb0c86e83f2f6334af236374b041126c8c54
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2024-06-21 11:11:01 +0200

    Added migration

---
+++ LectureService/Infrastructure/Migrations/20240621090000_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LectureService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lectures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StudyMaterialId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClassId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeacherId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lectures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudyMaterials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudyMaterials", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lectures");
+
+            migrationBuilder.DropTable(
+                name: "StudyMaterials");
+        }
+    }
+}
--- LectureService/Program.cs
+++ LectureService/Program.cs
@@ -1,25 +1,74 @@
+using LectureService.DomainServices;
+using EventLibrary;
+using LectureService.Infrastructure;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
+//using LectureService.Consumers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ILectureRepository, LectureRepository>();
+//builder.Services.AddScoped<IConsumer<ApplicantCreated>, ApplicantCreatedConsumer>();
+
+builder.Services.AddDbContext<LectureDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+//builder.Services.AddMassTransit(x =>
+//{
+//    x.AddConsumer<ApplicantCreatedConsumer>();
+//    x.UsingRabbitMq((context, cfg) =>
+//    {
+//        cfg.Host("rabbitmq", "/", h =>
+//        {
+//            h.Username("guest");
+//            h.Password("guest");
+//        });
+//        cfg.ReceiveEndpoint("Progress-applicant-created-queue", e =>
+//        {
+//            e.ConfigureConsumer<ApplicantCreatedConsumer>(context);
+//            e.Bind("applicant-created", x =>
+//            {
+//                x.RoutingKey = "#"; // wildcard to receive all messages
+//                x.ExchangeType = "topic";
+//            });
+//        });
+
+//    });
+//});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
-app.UseAuthorization();
-
-app.MapControllers();
+// Apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<LectureDbContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
