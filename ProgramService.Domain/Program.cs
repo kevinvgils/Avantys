5cf77890b1c456d2e68369772e7a6fa4a1696e34
Tim Aerts <t.aerts@hotmail.com>
2024-06-23 23:32:52 +0200

    Created ProgramService

---
+++ ProgramService.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgramService.Domain
+{
+    public class Program
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Modules { get; set; }
+
+        public Program() => Id = Guid.NewGuid();
+        public Program(string name) : this() => Name = name;    
+    }
+}
