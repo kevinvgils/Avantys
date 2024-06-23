35b4fb0215436e7fbeb7b7e3cad6a1d4793dbcef
Tim Aerts <t.aerts@hotmail.com>
2024-06-23 21:41:30 +0200

    IStudent&ITestRepository

---
+++ ProgressService/DomainServices/Interfaces/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProgressService.Domain;
+
+namespace ProgressService.DomainServices.Interfaces
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> getAllStudents();
+        Task<Student> createStudent(Student Student);
+    }
+}
---
+++ ProgressService/DomainServices/Interfaces/ITestRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProgressService.Domain;
+
+namespace TestService.DomainServices.Interfaces
+{
+    public interface ITestRepository
+    {
+        IEnumerable<Test> getAllTests();
+        Task<Test> createTest(Test Test);
+    }
+}
