b336f5c8f9698deeaf2b260d592a9eb2accc3a3b
Tim Aerts <t.aerts@hotmail.com>
2024-06-23 23:11:25 +0200

    Migration

--- ProgressService/Infrastructure/Migrations/20240623211027_Student&TestMigration.Designer.cs
+++ ProgressService/Infrastructure/Migrations/20240623211027_Student&TestMigration.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ProgressService.Infrastructure.Migrations
 {
     [DbContext(typeof(ProgressDbContext))]
-    partial class ProgressDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240623211027_Student&TestMigration")]
+    partial class StudentTestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
